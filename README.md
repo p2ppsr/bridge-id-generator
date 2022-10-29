816e6d38bdfb668c5397091bce32f9f563ad022a
Ty Everett <ty@tyweb.us>
2022-10-29 12:15:11 -0700

    Support configuration instructions

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-# generate-bridge-id
+# bridge-id-generator
 
 Generates a Bridge ID Private Key and Address
 
+## Overview
+
+This tool generates a new Bridge ID and Private Key. It will also generate the correct parameters for use within the `docker-compose.yml` file of an application which makes use of the new bridge.
+
 ## Usage
 
-```js
-npx generate-bridge-id
+```sh
+npm i -g bridge-id-generator
+npx bridge-id-generator
 ```
 
 ## License
--- index.js
+++ index.js
@@ -2,6 +2,34 @@
 const bsv = require('babbage-bsv')
 
 const privateKey = bsv.PrivateKey.fromRandom()
+const wif = privateKey.toWIF()
+const address = privateKey.toAddress().toString()
+const addressLower = address.toLowerCase()
 
-console.log(`Bridge ID Private Key: ${privateKey.toWIF()}`)
-console.log(`Bridge ID: ${privateKey.toAddress().toString()}`)
+console.log('\nGenerating a random Bridge ID address and private key...')
+console.log('Be sure to save the private key and keep it secret before use!\n')
+console.log('=======================')
+console.log(`Bridge ID Private Key:  ${wif}`)
+console.log(`Bridge ID:              ${address}`)
+console.log('=======================\n')
+console.log('To use this bridge, add the following snippet to your docker-compose.yml:')
+console.log(`
+  reader-${addressLower.slice(1, 5)}:
+    hostname: "reader-${addressLower}"
+    build: ../path/to/reader
+    environment:
+      BRIDGE: '${Buffer.from(JSON.stringify({
+        id: `${address}`
+      })).toString('base64')}'
+      PORT: 80
+      MONGODB_READ_CREDS: '${Buffer.from(`mongodb://bridgeport-mongo:3113/bridge_${address}`).toString('base64')}'
+      MONGODB_DATABASE: 'bridge_${address}'
+  transformer-${addressLower.slice(1, 5)}:
+    hostname: "transformer-${addressLower}"
+    build: ../path/to/transformer
+    environment:
+      PORT: 80
+      MONGODB_WRITE_CREDS: 'mongodb://bridgeport-mongo:3113/bridge_${address}'
+      MONGODB_DATABASE: 'bridge_${address}'
+  `)
+console.log('For support and documentation: https://projectbabbage.com/docs\n')
